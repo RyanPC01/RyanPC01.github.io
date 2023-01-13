fc95109370a04ca85a8f8a826efc125b1881e184
RyanPC01 <94211083+RyanPC01@users.noreply.github.com>
2023-01-13 16:33:53 -0500

    update
    update

---
+++ todoscript.js
@@ -0,0 +1,29 @@
+function addTask() {
+    var task = document.getElementById("task").value;
+    var taskList = document.getElementById("task-list");
+    var taskWithDeleteOption = document.createElement("li");
+    taskWithDeleteOption.innerHTML = task + "<button class='remove-btn'>X</button>";
+    taskList.appendChild(taskWithDeleteOption);
+    document.getElementById("task").value = "";
+  
+    var removeBtns = document.getElementsByClassName("remove-btn");
+    for (var i = 0; i < removeBtns.length; i++) {
+      removeBtns[i].addEventListener("click", function(event) {
+        this.parentElement.remove();
+      });
+    }
+  }
+  
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addTask();
+  });
+  
+  gapi.load('auth2', function() {
+    gapi.auth2.init({
+      client_id: 'your_client_id',
+    }).then(() => {
+      gapi.signin2.render('g-signin2');
+    });
+  });
